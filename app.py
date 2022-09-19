e29c27ccb5431b441fe29f15b4b095f5da585d87
Jack <jackdonnell74@gmail.com>
2022-09-19 21:35:24 +0700

    2

--- app.py
+++ app.py
@@ -9,11 +9,36 @@ db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sleekbook@127.0.0.1:5000'
 
 
-class Db(db.model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
-    password = db.Column(db.String(20))
+    username = db.Column(db.Varchar(20), unique=True, nullable=False)
+    email = db.Column(db.Varchar(40), unique=True, nullable=False)
+    password = db.Column(db.Varchar(20), unique=True, nullable=False)
+    image_file = db.column(db.String(20), nullable=False, default='default.jpeg')
+    user_ingredients = db.relationship('User_ingredients', backref= 'list', lazy=True)
 
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}, '{self.image_file})"
+
+class Ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredient_name = db.column(db.Varchar(30), unique=True, nullable= False)
+
+class User_ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredients = db.Column(db.Varchar(20), db.ForeginKey("ingredients.ingredient_name"))
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+
+class Drinks(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
+    recipe = db.Column(db.Varchar(1000))
+
+
+class Drink_ingredients(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    ingredients = db.Column(db.Varchar(20), db.ForeginKey("ingredients.ingredient_name"))
+    drink_id = db.Column(db.Integer, db.ForeignKey("drinks.id"))
 
 @app.route("/")
 def index():
