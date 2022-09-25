b5718e18d5a113a394752c3b0544451c44c57db0
Jack <jackdonnell74@gmail.com>
2022-09-25 23:38:33 +0800

    jsloop

--- scraper.py
+++ scraper.py
@@ -44,62 +44,27 @@ if __name__ == "__main__":
     if almondliqueur in user_selection:
         almondliqueur_check = driver.find_element(By.ID,'almondliqueur_checkbox')
         almondliqueur_check.click()
-#     if cherryliqueur in user_selection:
-#         cherryliqueur_check = driver.find_element(By.ID,'cherryliqueur_checkbox')
-#         cherryliqueur_check.click()
-#     if chocolateliqueur in user_selection:
-#         chocolateliqueur_check = driver.find_element(By.ID,'chocolateliqueur_checkbox')
-#         chocolateliqueur_check.click()
-#     if coffeeliqueur in user_selection:
-#         coffeeliqueur_check = driver.find_element(By.ID,'coffeeliqueur_checkbox')
-#         coffeeliqueur_check.click()
-#     if in user_selection:
-#         _check = driver.find_element(By.ID,'_checkbox')
-#         _check.click()
-#     if in user_selection:
-#         _check = driver.find_element(By.ID,'_checkbox')
-#         _check.click()
-#     if in user_selection:
-#         _check = driver.find_element(By.ID,'_checkbox')
-#         _check.click()
-#     if in user_selection:
-#         _check = driver.find_element(By.ID,'_checkbox')
-#         _check.click()
-#     if in user_selection:
-#         _check = driver.find_element(By.ID,'_checkbox')
-#         _check.click()
-# gin rum tequila vodka whiskey
-# import undetected_chromedriver as uc
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.support.wait import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-
-# if __name__ == '__main__':
-#     email = "#"
-#     password = "#"
-
-#     options = webdriver.ChromeOptions()
-#     #options.add_argument('proxy-server=106.122.8.54:3128')
-#     #options.add_argument(r'--user-data-dir=C:\Users\suppo\AppData\Local\Google\Chrome\User Data\Default')
-
-#     browser = uc.Chrome(
-#         options=options,
-#     )
-#     browser.get('https://accounts.google.com/signin/v2/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&service=mail&sacu=1&rip=1&hl=en&flowName=GlifWebSignIn&flowEntry=ServiceLogin')
-
-#     browser.find_element(By.ID, 'identifierId').send_keys(email)
-
-#     browser.find_element(
-#         By.CSS_SELECTOR, '#identifierNext > div > button > span').click()
-
-#     password_selector = "#password > div.aCsJod.oJeWuf > div > div.Xb9hP > input"
-
-#     WebDriverWait(browser, 10).until(
-#         EC.visibility_of_element_located((By.CSS_SELECTOR, password_selector)))
-
-#     browser.find_element(
-#         By.CSS_SELECTOR, password_selector).send_keys(password)
-
-#     browser.find_element(
-#         By.CSS_SELECTOR, '#passwordNext > div > button > span').click()
+    if cherryliqueur in user_selection:
+        cherryliqueur_check = driver.find_element(By.ID,'cherryliqueur_checkbox')
+        cherryliqueur_check.click()
+    if chocolateliqueur in user_selection:
+        chocolateliqueur_check = driver.find_element(By.ID,'chocolateliqueur_checkbox')
+        chocolateliqueur_check.click()
+    if coffeeliqueur in user_selection:
+        coffeeliqueur_check = driver.find_element(By.ID,'coffeeliqueur_checkbox')
+        coffeeliqueur_check.click()
+    if cola in user_selection:
+        cola_check = driver.find_element(By.ID,'cola_checkbox')
+        cola_check.click()
+    if in user_selection:
+        _check = driver.find_element(By.ID,'_checkbox')
+        _check.click()
+    if in user_selection:
+        _check = driver.find_element(By.ID,'_checkbox')
+        _check.click()
+    if in user_selection:
+        _check = driver.find_element(By.ID,'_checkbox')
+        _check.click()
+    if in user_selection:
+        _check = driver.find_element(By.ID,'_checkbox')
+        _check.click()
