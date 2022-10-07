74e57105b4f2d4952cf31beeb04824eabf129471
Jack <jackdonnell74@gmail.com>
2022-10-07 20:08:34 +0700

    1

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, logout_user, current_user
@@ -18,6 +17,32 @@ login_manager = LoginManager()
 login_manager.init_app(app)
 login_manager.login_view = 'login'
 
+user_ingredients = db.Table('user_ingredients',
+    db.Column(db.Integer, primary_key=True),
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('ingredients_id', db.Integer, db.ForeignKey('ingredients.id')),
+)
+    
+drinks_ingredients = db.Table('drinks_ingredients',
+    db.Column(db.Integer, primary_key=True),
+    db.Column('drinks_id', db.Integer, db.ForeignKey('drinks.id')),
+    db.Column('ingredients_id', db.Integer, db.ForeignKey('ingredients.id'))
+)
+
+class Ingredients(db.Model):
+    __tablename__ = 'ingredients'
+    id = db.Column(db.Integer, primary_key=True),
+    ingredient_name = db.column(db.Varchar(30), unique=True, nullable= False),
+    user_list = db.relationship('user', secondary=user_ingredients),
+    drinks_ings = db.relationship('drinks', secondary=drinks_ingredients)
+
+
+class Drinks(db.Model):
+    __tablename__ = 'drinks'
+    id = db.Column(db.Integer, primary_key=True),
+    drink_name = db.Column(db.Varchar(30), unique=True, nullable=False),
+    recipe = db.Column(db.Varchar(1000)),
+    drink_ings = db.relationship('ingredients', secondary=drinks_ingredients)
 
 @login_manager.user_loader
 def load_user(user_id):
@@ -45,7 +70,6 @@ class RegisterForm(FlaskForm):
         if existing_user_username:
             raise ValidationError(
                 'That username already exists. Please choose a different one.')
-
 
 class LoginForm(FlaskForm):
     username = StringField(validators=[
@@ -78,14 +102,13 @@ def login():
 def dashboard():
     return render_template('dashboard.html')
 
-variables = ['q']
 
 @app.route('/templates/results.html', methods=['GET', 'POST'])
 def results():
     if request.method == 'POST':
         print(request.form.getlist('checkbox'))
         return "done"
-    return render_template('results.html', variable = variables[0])
+    return render_template('results.html')
 
 @app.route('/logout', methods=['GET', 'POST'])
 @login_required
@@ -120,46 +143,23 @@ if __name__ == "__main__":
 
 # db = SQLAlchemy(app)
 
-# # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sleekbook@127.0.0.1:5000'
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sleekbook@127.0.0.1:5000'
 
 
-# # user_ingredients = db.Table('user_ingredients',
-# #     db.Column(db.Integer, primary_key=True),
-# #     db.Column('users_id', db.Integer, db.ForeignKey('users.id'), primary_key=True),
-# #     db.Column('ingredients_id', db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
-# # )
-    
-
-# # drinks_ingredients = db.Table('drinks_ingredients',
-# #     db.Column(db.Integer, primary_key=True),
-# #     db.Column('drinks_id', db.Integer, db.ForeignKey('drinks.id'), primary_key=True),
-# #     db.Column('ingredients_id', db.Integer, db.ForeignKey('ingredients.id'), primary_key=True)
-# # )
 
 
-# # class Users(db.Model):
-# #     __tablename__ = 'users'
-# #     user_id = db.relationship('Ingredients', secondary='user_ingredients', lazy='subquery', backref=db.backref('ingredients', lazy=True))
-# #     id = db.Column(db.Integer, primary_key=True)
-# #     username = db.Column(db.Varchar(20), unique=True, nullable=False)
-# #     email = db.Column(db.Varchar(40), unique=True, nullable=False)
-# #     password = db.Column(db.Varchar(20), unique=True, nullable=False)
-# #     image_file = db.column(db.String(20), nullable=False, default='default.jpeg')
+# class Users(db.Model):
+#     __tablename__ = 'users'
+#     user_id = db.relationship('Ingredients', secondary='user_ingredients', lazy='subquery', backref=db.backref('ingredients', lazy=True))
+#     id = db.Column(db.Integer, primary_key=True)
+#     username = db.Column(db.Varchar(20), unique=True, nullable=False)
+#     email = db.Column(db.Varchar(40), unique=True, nullable=False)
+#     password = db.Column(db.Varchar(20), unique=True, nullable=False)
+#     image_file = db.column(db.String(20), nullable=False, default='default.jpeg')
 
-# #     def __repr__(self):
-# #         return f"User('{self.username}', '{self.email}, '{self.image_file})"
+    # def __repr__(self):
+    #     return f"User('{self.username}', '{self.email}, '{self.image_file})"
         
-
-# # class Ingredients(db.Model):
-# #     __tablename__ = 'ingredients'
-# #     id = db.Column(db.Integer, primary_key=True)
-# #     ingredient_name = db.column(db.Varchar(30), unique=True, nullable= False)
-
-# # class Drinks(db.Model):
-# #     __tablename__ = 'drinks'
-# #     id = db.Column(db.Integer, primary_key=True)
-# #     user_id = db.Column(db.Integer, db.ForeignKey("user.id"))
-# #     recipe = db.Column(db.Varchar(1000))
 
 # @app.route("/")
 # def index():
@@ -174,7 +174,7 @@ if __name__ == "__main__":
 #     return render_template('signup.html')
 
 # if __name__ == '__main__':
-#     app.run(debug=True)
+    # app.run(debug=True)
 
 
 
